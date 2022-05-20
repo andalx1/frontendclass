1a646e2049b2ca04f99f6373b424ac5f36e9d8a6
andrezbenavidex@gmail.com <andrezbenavidex@gmail.com>
2022-05-20 13:02:25 -0500

    pract

---
+++ week5/practice/main.js
@@ -0,0 +1,11 @@
+function sumar(a,b){
+    return a + b;
+}
+ var resultado = sumar(5,3);
+ console.groupCollapsed(resultado);
+
+ function crearcadena(lenguajedeprogramacion){
+     return "mi meta es" + lenguajedeprogramacion;
+
+     
+ }
