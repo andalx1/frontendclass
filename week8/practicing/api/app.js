505d6dddd26e18db5c5d92749203a177d07863eb
andrezbenavidex@gmail.com <andrezbenavidex@gmail.com>
2022-06-11 17:42:42 -0500

    resp

--- week8/practicing/api/app.js
+++ week8/practicing/api/app.js
@@ -1,6 +1,11 @@
 const cargarPeliculas = async() => {
-    const respuesta = await fetch("https://api.themoviedb.org/3/movie/550?api_key=d9cea79d90fc09560fb9bc8ef5fd21b4");
+    try {
+        const respuesta = await fetch("https://api.themoviedb.org/3/movie/550?api_key=d9cea79d90fc09560fb9bc8ef5fd21b4");
 
     console.log(respuesta);
+    }catch(error){
+        console.log(error);
+    }
+    
 }
 cargarPeliculas();
