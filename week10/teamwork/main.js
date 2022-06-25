7c25c2a34d6710983a3d06a712e1a774ca9d82c6
andrezbenavidex@gmail.com <andrezbenavidex@gmail.com>
2022-06-25 16:41:50 -0500

    quake

--- week10/teamwork/main.js
+++ week10/teamwork/main.js
@@ -1,3 +1,15 @@
-const url = ('https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2019-01-01&endtime=2019-02-02')
+const url = 'https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2019-01-01&endtime=2019-02-02'
 
-function getJSON()
+function getJSON(url) {
+    return fetch(url)
+    .then(function(response){
+        if (!response.ok) {
+            throw Error(response.statusText);
+        } else {
+            return response.json();
+        }
+    })
+    .catch(function(error) {
+            console.log(error);
+    });
+    }
